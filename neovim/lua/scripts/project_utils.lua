82187c92cbff4bdfc83938a0c361ce8881bd23a6
Orson Routt <orson@routt.net>
2025-10-23 12:14:58 +1100

    NeoVim Grapple Changes
    Added 'cwd_branch' scope to Grapple (from help). Does 'git_branch' but
    cwd instead of git root directory.
    
    Made project definition include Grapple scope to switch to.

--- neovim/lua/scripts/project_utils.lua
+++ neovim/lua/scripts/project_utils.lua
@@ -21,6 +21,7 @@ M.load_project = function(name)
   if project[1] then vim.cmd("cd " .. project[1]) end
   if project[3] then vim.cmd("e " .. project[3]) end
   project[4] = os.time()
+  if project[5] then require("grapple").use_scope(project[5]) end
   require("scripts.fileio").save_file(vim.g.projects_file, projects)
 end
 
