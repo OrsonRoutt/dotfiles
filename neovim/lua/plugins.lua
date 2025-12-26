0623905703a5972c8cbf5a129930f6fd33b908a2
Orson Routt <orson@routt.net>
2025-12-27 02:03:16 +1100

    Fix

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -31,6 +31,7 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     lazy = false,
     dependencies = { "nvim-treesitter/nvim-treesitter-textobjects", "kevinhwang91/nvim-ufo" },
     build = ":TSUpdate",
