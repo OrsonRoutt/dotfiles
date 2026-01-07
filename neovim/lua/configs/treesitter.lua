7085eff3e97b12054e9b8d4240f1e8da8e5dea9a
Orson Routt <orson@routt.net>
2026-01-07 18:53:47 +1100

    Fixed nvim-treesitter
    Worked out I needed treesitter-cli for nvim-treesitter to work.
    Installed and refactored config into the config file. Haven't done
    textobjects yet.

--- neovim/lua/configs/treesitter.lua
+++ neovim/lua/configs/treesitter.lua
@@ -1,5 +1,7 @@
-return {
-  install = {
+local M = {}
+
+M.setup = function()
+  local parsers = {
     "vim", "lua", "vimdoc",
     "html", "css", "gdscript",
     "godot_resource", "gdshader",
@@ -8,5 +10,23 @@ return {
     "printf", "sql", "php",
     "typst", "comment", "python",
     "fish", "bash",
-  },
-}
+  }
+  require("nvim-treesitter").install(parsers)
+  local ft = {}
+  local n = 1
+  for _, v in ipairs(parsers) do
+    for _, f in ipairs(vim.treesitter.language.get_filetypes(v)) do
+      ft[n] = f
+      n = n + 1
+    end
+  end
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = ft,
+    callback = function()
+      vim.treesitter.start()
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
+  })
+end
+
+return M
